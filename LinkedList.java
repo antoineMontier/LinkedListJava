6352b172f1022ec4eec7250c48788ff94e390218
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-11-08 17:58:55 +0100

    added the copy builder

--- LinkedList.java
+++ LinkedList.java
@@ -19,6 +19,34 @@ public class LinkedList {
         head = null;
         tail = null;
     }
+    
+    /**
+     * create a LinkedList by copying the param list, same content but not the same adress
+     */
+    public LinkedList(LinkedList l){
+      if(l.getHead() == null){
+        head = null;
+        tail = null;
+      }else if(l.getHead().getnext() == null){
+        head = new Cell(l.getHead());
+        tail = new Cell(l.getHead());
+      }else{
+        head = new Cell(l.getHead());
+        Cell walker1 = new Cell(head);
+        Cell walker2 = new Cell(head.getnext());
+        //walker1.setnext(walker2);
+
+        while(walker2.getnext() != null){
+          walker1 = walker2;
+          walker2 = walker2.getnext();
+          Cell e = new Cell(walker2);
+          walker1.setnext(walker2);
+        }
+
+        walker1.setnext(walker2);
+        tail = walker2;
+      }
+    }
 
 
     /**
